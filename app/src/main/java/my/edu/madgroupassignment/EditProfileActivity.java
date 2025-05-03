6c4e4bbdff6332688ed7cabb21036f9cca9d40bc
Yong0206 <tansyong2002@gmail.com>
2025-05-03 08:41:44 +0800

    Update Commit

--- app/src/main/java/my/edu/madgroupassignment/EditProfileActivity.java
+++ app/src/main/java/my/edu/madgroupassignment/EditProfileActivity.java
@@ -7,7 +7,6 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.widget.*;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
